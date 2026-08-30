496600338ea59992b5b3259ad40e01635defd4e4
Amir maleki <amirmaleki21physics@gmail.com>
2026-08-30 11:01:35 +0330

    Add legacy packaging for JNI libraries in build.gradle.kts to ensure compatibility with existing native code. This change enhances the build process for Android applications.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,6 +31,12 @@ android {
         versionName = flutter.versionName
     }
 
+    packaging {
+        jniLibs {
+            useLegacyPackaging = true
+        }
+    }
+
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
